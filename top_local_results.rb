86b5a19977a6bff60e8185146ff1eb957e77e684
Ransom Byers <ransom.byers@gmail.com>
2017-12-18 22:45:18 -0800

    Start using the basics of the search api

--- top_local_results.rb
+++ top_local_results.rb
@@ -1,12 +1,17 @@
-locations = {
-	'Oakland',
+require 'google/apis/customsearch_v1'
+
+locations = [
 	'Piedmont',
-	'Kensington',
-}
+]
 
-services = {
+services = [
 	'home improvement',
-	'seismic retrofit',
-	'post and beam',
-}
+]
 
+
+searcher = Google::Apis::CustomsearchV1::CustomsearchService.new
+searcher.key = ENV['CUSTOM_SEARCH_API_KEY']
+
+result = searcher.list_cses("what", {cx: "001373072520138917942:w1tlsaygtke"})
+
+p result
