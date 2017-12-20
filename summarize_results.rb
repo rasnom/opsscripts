86e16e7efd0f4f8793b84bafababd103fb20e4ff
Ransom Byers <ransom.byers@gmail.com>
2017-12-19 16:35:34 -0800

    Add csv output of top links to summarize_results

--- summarize_results.rb
+++ summarize_results.rb
@@ -3,8 +3,13 @@ require 'json'
 summary = File.open('summary.csv','w')
 
 File.readlines('search_results_2017-12-19T22:49:43Z').each do |line|
+	toWrite = []
 	results = JSON.parse(line)
-	p results['queries']['request'][0]['searchTerms']
+	toWrite << results['queries']['request'][0]['searchTerms']
+	results['items'].each do |result|
+		toWrite << result['link']
+	end
+	summary.puts toWrite.join(',')
 end
 
 summary.close
