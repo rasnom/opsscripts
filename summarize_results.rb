8eed0ac1d62e43f32ca08cee5f563741bfabc31d
Ransom Byers <ransom.byers@gmail.com>
2017-12-28 02:17:36 -0800

    Summarize first results and collect data for the rest of the initialtial run

--- summarize_results.rb
+++ summarize_results.rb
@@ -2,7 +2,7 @@ require 'json'
 
 summary = File.open('summary.csv','w')
 
-File.readlines('search_results_2017-12-19T22:49:43Z').each do |line|
+File.readlines('search_results_2017-12-20T00:43:03Z').each do |line|
 	toWrite = []
 	results = JSON.parse(line)
 	toWrite << results['queries']['request'][0]['searchTerms']
